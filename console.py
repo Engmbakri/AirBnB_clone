e29d571747ebcf6da2146678ef458413856a8466
Emgmbakri <mrm507415@gmail.com>
2024-02-08 23:56:54 +0400

    cusotmization the console

--- console.py
+++ console.py
@@ -9,11 +9,11 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
-    #def __init__(self):
-    #    self.user = {}
+    valid_classes = ["BaseModel"]
 
     def do_EOF(self, line):
         """EOF command to exit the program"""
+        print("")
         return True
 
     def do_quit(self, line):
@@ -27,52 +27,72 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, args):
         """create a new method type <create> <class name>"""
         arg = args.split()
-        if not args:
+        if not arg:
             print("** class name missing **")
             return
         
-        class_name = arg[0]
-        if class_name != "BaseModel":
+        if arg[0] not in self.valid_classes:
             print("** class doesn't exist **")
         else:
             print(eval(arg[0])().id)   
         storage.save()
     
     def do_show(self, args):
-        """prints the string representation based on the class name and id
-        syntax show <class name> <id>"""
+        """
+        Prints the string representation based on the class name and id.
+        """
         arg = args.split()
-        
-        if not args:
+
+        if len(arg) == 0:
             print("** class name missing **")
-        elif arg[0] != "BaseModel":
+        elif arg[0] not in self.valid_classes:
             print("** class doesn't exist **")
         elif len(arg) < 2:
             print("** instance id missing **")
-        #elif arg[1] and arg[0] != "objects":
-            #print("** no instance found **") 
         else:
-            print("{}{}".format(arg[0], arg[1]))
+            objects = storage.all()
+
+            key = "{}.{}".format(arg[0], arg[1])
+            if key in objects:
+                print(objects[key])
+            else:
+                print("** no instance found **")
     
     def do_destroy(self, args):
         """Deletes an instance based on the class name and id 
         syntax destory <class_name> <id>"""
         arg = args.split()
-        if not args:
+        if len(arg) == 0:
             print("** class name missing **")
-        elif arg[0] != "BaseModel":
+        elif arg[0] not in self.valid_classes:
             print("** class doesn't exist **")
-        elif len(args) > 2:
+        elif len(args) < 2:
             print("** instance id missing **")
-        #elif arg[1] != "objects":
-            #print("** no instance found **")
         else:
-            del self.objects["{}{}".format(arg[0], arg[1])]
-        storage.save()
+            objects = storage.all()
+            key = "{}.{}".format(arg[0], arg[1])
+            if key in objects:
+                del objects[key]
+                storage.save()
+            else:
+                print("**no instance found**")
 
-    #def all(self, args):
+    def do_all(self, args):
+        """
+        Print the string representation of all instances or a specific class.
+        """
+        objects = storage.all()
+        arg = args.split()
 
-        
+        if len(arg) == 0:
+            for key, value in objects.items():
+                print(str(value))
+        elif arg[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        else:
+            for key, value in objects.items():
+                if key.split('.')[0] == arg[0]:
+                    print(str(value))
 
 
 if __name__ == "__main__":

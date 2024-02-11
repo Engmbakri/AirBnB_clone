ab34dc6d8fc932cca920a08ef520a33e396bc667
abdullahomg77 <abdullahomg77@gmail.com>
2024-02-10 17:36:18 -0800

    basemodel test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,39 @@
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.base_model = BaseModel()
+
+    def test_id_generation(self):
+        self.assertIsNotNone(self.base_model.id)
+        self.assertIsInstance(self.base_model.id, str)
+
+    def test_created_at_and_updated_at(self):
+        self.assertIsInstance(self.base_model.created_at, datetime)
+        self.assertIsInstance(self.base_model.updated_at, datetime)
+        self.assertEqual(self.base_model.created_at, self.base_model.updated_at)
+
+    def test_save_updates_updated_at(self):
+        old_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        self.assertNotEqual(self.base_model.updated_at, old_updated_at)
+
+    def test_to_dict(self):
+        obj_dict = self.base_model.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertIsInstance(obj_dict['created_at'], str)
+        self.assertIsInstance(obj_dict['updated_at'], str)
+        self.assertIsInstance(obj_dict['id'], str)
+
+    def test_string_representation(self):
+        expected_str = f"[BaseModel], ({self.base_model.id}), {self.base_model.__dict__}"
+        self.assertEqual(str(self.base_model), expected_str)
+
+
+if __name__ == '__main__':
+    unittest.main()
+

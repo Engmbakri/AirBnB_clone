790a0709aa74786fd6008040ac58d97f1d8cd4c5
abdullahomg77 <abdullahomg77@gmail.com>
2024-02-10 17:02:29 -0800

    Basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ module for the BaseModel class.
 """
 
 from datetime import datetime
-import uuid
+from uuid import uuid4
 import models
 
 
@@ -32,7 +32,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            self.id = str(uuid.uuid4())
 
         models.storage.new(self)
 
@@ -52,7 +51,6 @@ class BaseModel:
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
-        obj_dict['id'] = self.id
         return obj_dict
 
     def __str__(self):

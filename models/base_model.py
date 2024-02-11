e7bd70f298faefcc1b425abfa088abf9876477a7
abdullahomg77 <abdullahomg77@gmail.com>
2024-02-10 17:09:25 -0800

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -32,7 +32,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            self.id = str(uuid.uuid4())
 
         models.storage.new(self)
 
@@ -52,7 +51,6 @@ class BaseModel:
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
-        obj_dict['id'] = self.id
         return obj_dict
 
     def __str__(self):

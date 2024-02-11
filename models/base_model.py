e4663064e2ebaa4cb1a604acb9fed7dbd11c354a
Emgmbakri <mrm507415@gmail.com>
2024-02-11 18:35:50 +0400

    testing

--- models/base_model.py
+++ models/base_model.py
@@ -29,6 +29,7 @@ class BaseModel:
                     else:
                         setattr(self, key, value)
         else:
+            time_format = "%Y-%m-%dT%H:%M:%S.%f"
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
